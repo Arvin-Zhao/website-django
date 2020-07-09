bfe9426f1d92097b7c93bf67956ce23c8f9b43d0
Arvin-Zhao <870173177@qq.com>
2020-07-09 16:57:23 +0800

    Update urls.py

--- filebrowser/urls.py
+++ filebrowser/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="file_browser_index")
+    path("", views.index, name="file_browser_index"),
+    path("?path=<str:path>", views.index, name="file_browser_index")
 ]
